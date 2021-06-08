26c050c0cceccf9a2a8ad278df39d2c1c6ac0f70
Alexander Böhm <3983539+mheob@users.noreply.github.com>
2021-06-08 14:39:55 +0200

    Improve the exporting of the theme package (#44)

--- packages/theme/src/foundations/fonts.ts
+++ packages/theme/src/foundations/fonts.ts
@@ -1,10 +1,18 @@
+import { theme } from '@chakra-ui/react'
+
 /**
  * Please ensure to add the `Lato` font to the project.
  *
  * @see https://chakra-ui.com/guides/using-fonts
  */
 export const fonts = {
+  ...theme.fonts,
   heading: `Lato, -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"`,
   body: `Lato, -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"`,
   mono: `SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace`,
+}
+
+export const fontSizes = {
+  ...theme.fontSizes,
+  '2xs': '0.625rem',
 }
