7b8990052ffbf17c3e789c2fdc477d5930c4af6a
Alexander Böhm <3983539+mheob@users.noreply.github.com>
2021-06-09 16:13:13 +0200

    Add DarkMode Switch to the storyboard
    Also use dark mode for the general storybook, add a11y plugin and add rtl switch.

---
+++ .storybook/preview.tsx
@@ -0,0 +1,69 @@
+import '@fontsource/lato'
+
+import {
+  Center,
+  ChakraProvider,
+  Flex,
+  IconButton,
+  useColorMode,
+  useColorModeValue,
+} from '@chakra-ui/react'
+import { theme } from '@easyfeedback/theme'
+import { StoryContext } from '@storybook/react'
+import * as React from 'react'
+import { FaMoon, FaSun } from 'react-icons/fa'
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+}
+
+/** Add global context for RTL-LTR switching */
+export const globalTypes = {
+  direction: {
+    name: 'Direction',
+    description: 'Direction for layout',
+    defaultValue: 'LTR',
+    toolbar: {
+      icon: 'globe',
+      items: ['LTR', 'RTL'],
+    },
+  },
+}
+
+const ColorModeToggleBar = () => {
+  const { toggleColorMode } = useColorMode()
+  const SwitchIcon = useColorModeValue(FaMoon, FaSun)
+  const nextMode = useColorModeValue('dark', 'light')
+
+  return (
+    <Flex justify="flex-end" mb={4}>
+      <IconButton
+        size="md"
+        fontSize="lg"
+        aria-label={`Switch to ${nextMode} mode`}
+        variant="ghost"
+        color="current"
+        marginLeft="2"
+        onClick={toggleColorMode}
+        icon={<SwitchIcon />}
+      />
+    </Flex>
+  )
+}
+
+const withChakra = (StoryFn: Function, context: StoryContext) => {
+  const { direction } = context.globals
+  const dir = direction.toLowerCase()
+  return (
+    <ChakraProvider theme={{ ...theme, direction: dir }}>
+      <div dir={dir} id="story-wrapper">
+        <ColorModeToggleBar />
+        <Center my="16">
+          <StoryFn />
+        </Center>
+      </div>
+    </ChakraProvider>
+  )
+}
+
+export const decorators = [withChakra]
