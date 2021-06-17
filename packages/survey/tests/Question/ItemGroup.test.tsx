d0b853db7e64e7257ed40c5a2ad920a2196f29fb
Jury Giannelli <jgiannelli@easy-feedback.de>
2021-06-17 15:10:39 +0200

    move props "up"

--- packages/survey/tests/Question/ItemGroup.test.tsx
+++ packages/survey/tests/Question/ItemGroup.test.tsx
@@ -2,34 +2,35 @@ import { render, testA11y } from '@easyfeedback/test-utils'
 
 import { ItemGroup, ItemGroupProps } from '../../src'
 
-describe('ItemGroup component', () => {
-  const props: ItemGroupProps = {
-    name: 'group_name',
-    type: 'checkbox',
-    options: [
-      {
-        label: 'Value 1',
-        value: 'val1',
-      },
-      {
-        label: 'Value 2',
-        value: 'val2',
-      },
-      {
-        label: 'Value 3',
-        value: 'val3',
-      },
-    ],
-    direction: 'vertical',
-    withLabels: false,
-  }
+const mockDefaultProps: ItemGroupProps = {
+  name: 'group_name',
+  itemType: 'checkbox',
+  activeColor: '#990033',
+  options: [
+    {
+      label: 'Value 1',
+      value: 'val1',
+    },
+    {
+      label: 'Value 2',
+      value: 'val2',
+    },
+    {
+      label: 'Value 3',
+      value: 'val3',
+    },
+  ],
+  direction: 'vertical',
+  withLabels: false,
+}
 
+describe('ItemGroup component', () => {
   it('passes a11y test', async () => {
-    await testA11y(<ItemGroup {...props} />)
+    await testA11y(<ItemGroup {...mockDefaultProps} />)
   })
 
   it('renders correctly', () => {
-    const { getByTestId } = render(<ItemGroup {...props} />)
+    const { getByTestId } = render(<ItemGroup {...mockDefaultProps} />)
     expect(getByTestId('ItemGroup')).toBeTruthy()
   })
 })
