f72541f8dd5ac36e14c4e9dae5f1a742bbcfe932
Alexander Böhm <3983539+mheob@users.noreply.github.com>
2021-06-21 13:03:03 +0200

    Use babel for the build project

--- jest.config.js
+++ jest.config.js
@@ -6,9 +6,9 @@ module.exports = {
     '^.+\\.(ts|tsx)?$': 'ts-jest/dist',
   },
   modulePathIgnorePatterns: ['<rootDir>/docs-build', '<rootDir>/tools'],
-  transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|jsx|ts|tsx)$'],
+  transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|jsx)$'],
   coveragePathIgnorePatterns: ['<rootDir>/docs-build', '<rootDir>/tools'],
-  setupFilesAfterEnv: ['./config/jest-setup.ts'],
+  setupFilesAfterEnv: ['@testing-library/jest-dom'],
   globals: {
     'ts-jest': {
       tsconfig: 'tsconfig.json',
