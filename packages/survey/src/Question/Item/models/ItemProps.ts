c3ecb4e383058f0e65a93286580d126c34512b34
Jury Giannelli <jgiannelli@easy-feedback.de>
2021-06-23 14:25:13 +0200

    fix props comments

--- packages/survey/src/Question/Item/models/ItemProps.ts
+++ packages/survey/src/Question/Item/models/ItemProps.ts
@@ -1,21 +1,21 @@
 export type OptionObj = {
-  /* The item value for the input field */
+  /** The item value for the input field. */
   value: string
-  /* The label we should display (when withLabels is active) */
+  /** The label we should display (when withLabels is active). */
   label: string
-  /* The image we should display (when withImages is active) */
+  /** The image we should display (when withImages is active). */
   imageSrc?: string
 }
 
 export type ItemProps = {
-  /* The item type we should use */
+  /** The item type we should use */
   itemType: 'checkbox' | 'radio'
-  /* The color used when checked is true */
+  /** The color used when checked is true. */
   activeColor: string
-  /* This is chakra controlled value */
+  /** This is chakra controlled value. */
   isChecked?: boolean
-  /* The option to show or hide labels */
+  /** The option to show or hide labels. */
   withLabels: boolean
-  /* The option to show or hide images */
+  /** The option to show or hide images. */
   withImages: boolean
 } & Pick<OptionObj, 'value' | 'label' | 'imageSrc'>
